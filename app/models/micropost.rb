3ceda4a164fadebd4d0494efff617caac0302eb7
Valentine <valentinetan91@gmail.com>
2022-06-28 10:21:54 +0000

    adding validation to resources

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,5 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  validates :user, :content, presence: true
+  validates :content, length: { maximum: 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :micropost_posts
+  validates :name, :first_name, :last_name, :email, presence: true
+  validates :name, length: { maximum: 20 }
 end
